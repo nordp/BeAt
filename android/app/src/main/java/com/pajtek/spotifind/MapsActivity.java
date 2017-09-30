6329687162a207151d8ee0578de7297426fd4df4
Simon Moos <simon.moos95@gmail.com>
2017-09-30 10:08:02 +0200

    Fetch current location every 5 seconds

--- android/app/src/main/java/com/pajtek/spotifind/MapsActivity.java
+++ android/app/src/main/java/com/pajtek/spotifind/MapsActivity.java
@@ -1,19 +1,38 @@
 package com.pajtek.spotifind;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.Arrays;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private final static int REQUEST_LOCATION_CODE = 0x00000001;
+    private final static int FETCH_LOCATION_EVERY_X_MS = 5_000;
+
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationClient;
+
+    // Updated regularly
+    Location currentLocation = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +42,64 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        boolean canFetchLocationNow = setupLocationServices();
+        if (canFetchLocationNow) {
+            // To be a bit responsive when opening the app
+            fetchCurrentLocation();
+        }
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                fetchCurrentLocation();
+            }
+        }, FETCH_LOCATION_EVERY_X_MS, FETCH_LOCATION_EVERY_X_MS);
+    }
+
+    private boolean setupLocationServices() {
+
+        if (mFusedLocationClient == null) {
+            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        }
+
+        boolean canAccess = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+        if (!canAccess) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_CODE);
+        }
+
+        // Can I access locations right now, or do I have to wait til permission is granted later?
+        return canAccess;
+    }
+
+    private void fetchCurrentLocation() {
+        // Can't do anything here if we don't have the permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        mFusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                currentLocation = location;
+                currentLocationUpdated(location);
+                Log.d("MapsActivity", "New location fetched: " + location.toString());
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_LOCATION_CODE) {
+            if (Arrays.asList(permissions).contains(Manifest.permission.ACCESS_FINE_LOCATION) && Arrays.asList(permissions).contains(Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                // Permission granted
+            }
+        }
+    }
+
+    private void currentLocationUpdated(Location location) {
+        // TODO
+        // TODO Here we can do stuff like query for the next applicable song etc.
+        // TODO
     }
 
 
